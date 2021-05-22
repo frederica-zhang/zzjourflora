1cdb9914ba586a7f8c5816a837f72ff268112ff5
frederica-zhang <zzha5483@uni.sydney.edu.au>
2021-05-22 21:20:59 +0800

    Update upload.rb

--- campusfloraweb-master/campusfloraweb-master/app/models/upload.rb
+++ campusfloraweb-master/campusfloraweb-master/app/models/upload.rb
@@ -1,28 +1,4 @@
-require 'csv'
-class Upload
-  class << self
-    def self.import(file)
-      CSV.foreach(file.path, headers: true) do |row|
-        upload = Upload.new
-        upload.save! row.to_hash
-      end
-    end
-
-    # def convert_save(model_name, csv_data)
-    #   csv_file = csv_data.read
-    #   CSV.parse(csv_file) do |row|
-    #     target_model = model_name.classify.constantize
-    #     new_object = target_model.new
-    #     column_iterator = -1
-    #     target_model.column_names.each do |key|
-    #       column_iterator += 1
-    #       unless key == "ID"
-    #         value = row[column_iterator]
-    #         new_object.send "#{key}=", value
-    #       end
-    #     end
-    #     new_object.save
-    #   end
-    # end
-  end
+class Upload  < ActiveRecord::Base
+  # devise :database_authenticatable, 
+  # :recoverable, :rememberable, :trackable, :validatable
 end
