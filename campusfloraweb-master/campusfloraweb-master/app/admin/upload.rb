84a1b7f2dfeb58af45a70ec31f7506b9c11bd1ca
frederica-zhang <zzha5483@uni.sydney.edu.au>
2021-05-26 19:40:10 +0800

    1

--- campusfloraweb-master/campusfloraweb-master/app/admin/upload.rb
+++ campusfloraweb-master/campusfloraweb-master/app/admin/upload.rb
@@ -2,9 +2,4 @@ ActiveAdmin.register Upload do
     permit_params :choose, :upload
     active_admin_importable
   
-    index do
-      column :choose
-      column :upload
-      actions
-    end
   end
