d46dc2b06f93e8047d62ac02d02b3577bbd67ce3
frederica-zhang <zzha5483@uni.sydney.edu.au>
2021-05-26 19:53:52 +0800

    3

--- campusfloraweb-master/campusfloraweb-master/app/admin/upload.rb
+++ campusfloraweb-master/campusfloraweb-master/app/admin/upload.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register Upload do
-    permit_params :choose, :upload
     active_admin_importable
   
   end
--- campusfloraweb-master/campusfloraweb-master/app/models/upload.rb
+++ campusfloraweb-master/campusfloraweb-master/app/models/upload.rb
@@ -1,4 +1,5 @@
 class Upload  < ActiveRecord::Base
+  has_many :species
   # devise :database_authenticatable, 
   # :recoverable, :rememberable, :trackable, :validatable
 end
